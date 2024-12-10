f70c4e7adc26cb565228f7882937702d0090456d
vedrakholia32 <vedrakholia32@gmail.com>
2024-12-10 18:39:43 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
+import notificationRoutes from "./routes/notification.route.js"
 import { configDotenv } from "dotenv"
 import connectMongo from "./db/connectDb.js"
 import cookieParser from "cookie-parser"
@@ -22,6 +23,7 @@ app.use(express.urlencoded({extended:true})) // to parse from data(urlencoded)
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/posts", postRoutes)
+app.use("/api/notification", notificationRoutes)
 app.use(cookieParser())
 
 app.listen(process.env.PORT, () => {
